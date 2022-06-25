4194304d84b12fd5d0451c3549b8c404810989fc
Fival <thefivalco@gmail.com>
2022-06-25 08:09:37 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿byte bite = 5;
+bite = 6;
+Console.WriteLine(bite);
+
+short shorty = 100;
+Console.WriteLine(shorty);
+
+int inty = 15;
+Console.WriteLine(inty);
+
+long longy = 150;
+Console.WriteLine(longy);
+
+sbyte sbytey = 100;
+Console.WriteLine(sbytey);
+
+ushort ushorty = 30;
+Console.WriteLine(ushorty);
+
+uint uinty = 2;
+Console.WriteLine(uinty);
+
+ulong ulongy = 75;
+Console.WriteLine(ulongy);
+
+// char bLetter = "b";
+// Console.WriteLine(bLetter);
+
+string message = "this is a message";
+Console.WriteLine(message);
+
+float first = 3.2f;
+Console.WriteLine(first);
+
+double second = 3.3;
+Console.WriteLine(second);
+
+decimal third = 3.67m;
+Console.WriteLine(third);
+
+bool truthy = true;
+Console.WriteLine(truthy);
